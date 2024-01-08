25bd7c56e6c06a8805307023691a50c0b8948e85
AlessandroTacconi <alessandro.tacconi.97@gmail.com>
2024-01-08 17:26:33 +0100

    feat: fine

--- badwords.php
+++ badwords.php
@@ -1,3 +1,8 @@
+    <?php
+        $paragrafo = $_POST["paragrafo"];
+        $parolaDaCensurare = $_POST["parola"];
+        $paragrafoCensurato = str_replace($parolaDaCensurare, '***', $paragrafo);
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,5 +11,12 @@
     <title>Badwords</title>
 </head>
 <body>
+    <p><?php echo $paragrafo ?></p>
+    <p><?php echo strlen($paragrafo); ?></p>
+    <p><?php echo $paragrafoCensurato ?></p>
+    <p><?php echo strlen($paragrafoCensurato); ?></p>
+
 </body>
 </html>
+
+
--- index.php
+++ index.php
@@ -3,8 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>PHP Badwords</title>
 </head>
 <body>
+    <!-- form per inserire il testo e la parola da censurare -->
+    <form action="badwords.php" method="post">
+        <label for="paragrafo">Inserisci il paragrafo:</label>
+        <textarea name="paragrafo" id="paragrafo" cols="30" rows="10">Si sta come d’autunno sugli alberi le foglie
+        </textarea>
+        <label for="parola">Inserisci la parola da censurare:</label>
+        <input type="text" name="parola" id="parola">
+        <button>invia</button>
+    </form>
 </body>
 </html>
